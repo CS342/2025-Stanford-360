e950b5448accbe8b01112e5d2a43f15cc2fd44f4
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2025-01-24 11:40:58 -0800

    Setup the CS234 2025 Stanford 360 Project  (#1)
    # Setup the CS234 2025 QuanBioSync Project
    
    ## :gear: Release Notes 
    - Setup the CS234 2025 QuanBioSync Project
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).

---
+++ Stanford360/Stanford360.swift
@@ -0,0 +1,37 @@
+//
+// This source file is part of the Stanford 360 based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import Spezi
+import SpeziFirebaseAccount
+import SpeziViews
+import SwiftUI
+
+
+@main
+struct Stanford360: App {
+    @UIApplicationDelegateAdaptor(Stanford360Delegate.self) var appDelegate
+    @AppStorage(StorageKeys.onboardingFlowComplete) var completedOnboardingFlow = false
+
+    
+    var body: some Scene {
+        WindowGroup {
+            ZStack {
+                if completedOnboardingFlow {
+                    HomeView()
+                } else {
+                    EmptyView()
+                }
+            }
+                .sheet(isPresented: !$completedOnboardingFlow) {
+                    OnboardingFlow()
+                }
+                .testingSetup()
+                .spezi(appDelegate)
+        }
+    }
+}
