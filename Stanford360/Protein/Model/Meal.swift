6edb4c9730c99b9a882b6aaa220a1568c2b795b9
Turquoise-T <ecyoyo1125@gmail.com>
2025-03-03 00:22:58 -0800

    add imageURL

--- Stanford360/Protein/Model/Meal.swift
+++ Stanford360/Protein/Model/Meal.swift
@@ -13,20 +13,20 @@ struct Meal: Identifiable, Codable, @unchecked Sendable {
 	@DocumentID var id: String?
 	var name: String // Name of the meal, e.g., "Chicken breast" or "Protein shake"
 	var proteinGrams: Double // Amount of protein in the meal (in grams)
-	// var imageURL: String? // Optional URL of the meal's image
+	var imageURL: String? // Optional URL of the meal's image
 	var timestamp: Date // Time when the meal was consumed
 	
 	init(
 		name: String,
 		proteinGrams: Double,
-		// imageURL: String? = nil,
+		imageURL: String? = nil,
 		timestamp: Date = Date(),
 		id: String? = UUID().uuidString
 	) {
 		self.id = id
 		self.name = name
 		self.proteinGrams = proteinGrams
-		// self.imageURL = imageURL
+		self.imageURL = imageURL
 		self.timestamp = timestamp
 	}
 }
