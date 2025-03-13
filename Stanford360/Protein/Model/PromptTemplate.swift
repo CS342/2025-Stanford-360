3f993872cbf44538940dba710c861dfd8e9a76a0
Turquoise-T <ecyoyo1125@gmail.com>
2025-03-12 20:51:31 -0700

    fix bugs in LLM

--- Stanford360/Protein/Model/PromptTemplate.swift
+++ Stanford360/Protein/Model/PromptTemplate.swift
@@ -15,7 +15,13 @@ class ProteinPromptConstructor: ObservableObject {
     func constructPrompt(mealName: String) -> String {
         let prompt = """
         You are an expert in nutritional science with a focus on dietary needs for children aged 10-15.
-
+        Here are some important information that you can refer:
+        1. Those belong to Meats, Poultry, and Fish usually around 6-10 grams.
+        2. Those belong to Soy and Vegetable Protein usually around 3-13 grams.
+        3. Those belong to Legumes and Nuts usually around 5-10 grams.
+        4. Those belong to Milk and Dairy usually around 10 grams.
+        5. Those belong to Vegetables usually around 2 grams.
+        6. Those belong to Grains usually around 5-10 grams.
         Task:
         1. Analyze the meal name: "\(mealName)"
         2. Determine the appropriate protein content (in grams) based on nutritional standards for this age group.
